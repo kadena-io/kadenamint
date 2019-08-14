999e3b8b0823c71248501fbcc5c7fc10300218ea
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2019-08-14 20:45:37 +0100

    Implement minimal abci app

--- backend/src/Backend.hs
+++ backend/src/Backend.hs
@@ -5,11 +5,39 @@
 {-# LANGUAGE TypeFamilies #-}
 module Backend where
 
+import Control.Monad.IO.Class                           (MonadIO(..))
+
+import Network.ABCI.Server                              (serveApp)
+import Network.ABCI.Server.App                          (App(..), Request(..))
+import Network.ABCI.Server.Middleware.RequestLogger     (mkLogStdoutDev)
+
 import Common.Route
 import Obelisk.Backend
 
 backend :: Backend BackendRoute FrontendRoute
 backend = Backend
-  { _backend_run = \serve -> serve $ const $ return ()
+  { _backend_run = \_serve -> liftIO runABCI
   , _backend_routeEncoder = backendRouteEncoder
   }
+
+runABCI :: IO ()
+runABCI = do
+  print "Launching application"
+  logger <- mkLogStdoutDev
+  serveApp $ logger app
+
+type EffectsT = IO
+
+app :: App EffectsT
+app = App $ \req -> case req of
+  RequestEcho _ -> pure def
+  RequestFlush _ -> pure def
+  RequestInfo _ -> pure def
+  RequestSetOption _ -> pure def
+  RequestInitChain _ -> pure def
+  RequestQuery _ -> pure def
+  RequestBeginBlock _ -> pure def
+  RequestCheckTx _ -> pure def
+  RequestDeliverTx _ -> pure def
+  RequestEndBlock _ -> pure def
+  RequestCommit _ -> pure def
--- default.nix
+++ default.nix
@@ -8,9 +8,31 @@
   }
 }:
 with obelisk;
-project ./. ({ ... }: {
-  android.applicationId = "systems.obsidian.obelisk.examples.minimal";
-  android.displayName = "Obelisk Minimal Example";
-  ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
-  ios.bundleName = "Obelisk Minimal Example";
-})
+project ./. ({ pkgs, hackGet, ... }:
+  let
+    hs-abci = hackGet ./dep/hs-abci;
+
+    extra-build-inputs = with pkgs; {
+      hs-abci-types = [protobuf];
+    };
+
+    addBuildInputs = inputs: { buildInputs ? [], ... }: { buildInputs = inputs ++ buildInputs; };
+
+  in {
+    android.applicationId = "systems.obsidian.obelisk.examples.minimal";
+    android.displayName = "Obelisk Minimal Example";
+    ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
+    ios.bundleName = "Obelisk Minimal Example";
+
+    packages =  {
+      hs-abci-server = hs-abci + /hs-abci-server;
+      hs-abci-types  = hs-abci + /hs-abci-types;
+      hs-abci-extra  = hs-abci + /hs-abci-extra;
+    };
+
+    overrides = pkgs.lib.foldr pkgs.lib.composeExtensions  (_: _: {}) [
+      (import hs-abci {}).overrides
+      (self: super: builtins.mapAttrs (name: deps: super.${name}.overrideAttrs (addBuildInputs deps)) extra-build-inputs)
+    ];
+  }
+)
