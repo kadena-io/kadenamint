07c43de624985c6c10134c8db09a75c5a0308f9b
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2020-03-02 22:54:29 +0000

    Use public thunk

--- dep/kpkgs/default.nix
+++ dep/kpkgs/default.nix
@@ -1,13 +1,8 @@
 # DO NOT HAND-EDIT THIS FILE
-let fetch = {url, rev, branch ? null, sha256 ? null, fetchSubmodules ? false, private ? false, ...}:
-  let realUrl = let firstChar = builtins.substring 0 1 url; in
-    if firstChar == "/" then /. + url
-    else if firstChar == "." then ./. + url
-    else url;
-  in if !fetchSubmodules && private then builtins.fetchGit {
-    url = realUrl; inherit rev;
-    ${if branch == null then null else "ref"} = branch;
-  } else (import <nixpkgs> {}).fetchgit {
-    url = realUrl; inherit rev sha256;
+let fetch = { private ? false, fetchSubmodules ? false, owner, repo, rev, sha256, ... }:
+  if !fetchSubmodules && !private then builtins.fetchTarball {
+    url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz"; inherit sha256;
+  } else (import <nixpkgs> {}).fetchFromGitHub {
+    inherit owner repo rev sha256 fetchSubmodules private;
   };
-in import (fetch (builtins.fromJSON (builtins.readFile ./git.json)))
+in import (fetch (builtins.fromJSON (builtins.readFile ./github.json)))
