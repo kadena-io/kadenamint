0629c2737ad5542596ef3d2120a4616f9aa1d95c
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2020-04-06 23:19:09 +0100

    Bump tendermint

--- dep/tendermint.nix
+++ dep/tendermint.nix
@@ -2,22 +2,14 @@
 
 buildGoModule rec {
   pname = "tendermint";
-  version = "0.32.4";
+  version = "0.32.8";
 
   src = fetchFromGitHub {
     owner = "tendermint";
     repo = pname;
     rev = "v${version}";
-    sha256 = "19kyzya8hcnvl5ndnlc06pziabglzbwpl20r238kcrld3apnz26k";
+    sha256 = "1bk9hb8whvlvxn8ijrp7rg6m83wxj4ipp5v36yr47ibqirbngh5s";
   };
 
-  modSha256 = "1j48iz5gnjpsxgz3xffs9my2injvsdqhz9balwn3z99y34zyghmh";
-
-  meta = with stdenv.lib; {
-    description = "Byzantine-Fault Tolerant State Machines. Or Blockchain, for short.";
-    homepage = https://tendermint.com/;
-    license = licenses.asl20;
-    maintainers = with maintainers; [ alexfmpe ];
-    platforms = platforms.linux ++ platforms.darwin;
-  };
+  modSha256 = "08f03haxzpi57gaxymsbzs0nbbgnf6z4gmpal476xy3gvc0dyi3r";
 }
